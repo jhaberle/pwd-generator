4075b328eee5ca10da5ff6556acbe5203106bae4
Jake Haberle <jake.haberle@gmail.com>
2020-07-10 22:05:42 -0600

    added new vars to js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ---------------Global Vars-------------------------
+
 var underCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -5,3 +7,20 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var confirm4 = confirm("Do you want only 4 characters in your password?")
+
+var confirm8 = confirm("Do you want 8 characters in your password?")
+
+var confirm12 = confirm("do you want 12 characters in your password?")
+
+var confirm16 =confirm("do you want 16 characters in your password?")
+
+var finalPassword = "";
+
+
+// --------------------------------------------------
+
+if (confirm4) { 
+
+}
