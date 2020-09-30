e201021df3d41f2b811a18213e451a0c313172f1
Jake Haberle <jake.haberle@gmail.com>
2020-09-29 22:46:34 -0600

    updated if statements and added new empty array for password

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var finalPassword = [];
 
+var passwordJoin = [];
+
 
 // --------------------------------------------------
 
@@ -18,13 +20,30 @@ var finalPassword = [];
 
 function generateNumber() {
     alert("Password Generator - Answer the following questions to determine specific aspects of your new password to be created.");
-    var confirmOpen = confirm("how may characters would you like your password to be (must be between 8 and 128)");
+    var confirmOpen = prompt("how may characters would you like your password to be (must be between 8 and 128)");
     console.log(confirmOpen);
 
     if (confirmOpen < 8 || confirmOpen > 128) { 
-        alert("Your password must be between 8 and 128 characters long");
-        return;
-    }
+        alert("Your password must be between 8 and 128 characters long");return
+       
+    } 
+    else if (confirmOpen > 8 || confirmOpen < 128)
+        console.log("your number is between 8 and 128");
+    
+        var underCaseConfirm = confirm("Do you want undercase letters in your password?");
+        console.log(underCaseConfirm);
+
+        var upperCaseConfirm = confirm("Do you want capitalized letters in your password?");
+        console.log(upperCaseConfirm);
+
+        var specCharConfirm = confirm("Do you want special characters in your password?");
+        console.log(specCharConfirm);
+
+        var numberConfirm = confirm("Do you want numbers in your password?");
+        console.log(numberConfirm);
+    
+    
+    
     // } else if 
     //     (confirmOpen > 128) {
     //     alert("your password must be less than 128 characters");
@@ -34,40 +53,44 @@ function generateNumber() {
 
     for (var i = 0; i < confirmOpen; i++) {
         if (underCaseConfirm) {
-            var newUndercase = undercase[Math.floor(math.random() * undercase.length)];
-            finalPassword.push(newUndercase);
+            var newUndercase = underCase[Math.floor(Math.random() * underCase.length)];
+            passwordJoin.push(newUndercase);
+            // console.log(newUndercase);
         } else {
             return;
         }
         if (upperCaseConfirm) {
-            var newUppercase = undercase[Math.floor(math.random() * undercase.length)];
-            finalPassword.push(newUppercase);
+            var newUppercase = upperCase[Math.floor(Math.random() * upperCase.length)];
+            passwordJoin.push(newUppercase);
+            // console.log(newUppercase);
         } else {
             return;
         }
         if (specCharConfirm) {
-            var newSpecialChar = undercase[Math.floor(math.random() * undercase.length)];
-            finalPassword.push(newSpecialChar);
+            var newSpecialChar = specChar[Math.floor(Math.random() * specChar.length)];
+            passwordJoin.push(newSpecialChar);
+            // console.log(newSpecialChar);
         } else {
             return;
         }
         if (numberConfirm) {
-            var newNumber = undercase[Math.floor(math.random() * undercase.length)];
-            finalPassword.push(newNumber);
+            var newNumber = number[Math.floor(Math.random() * number.length)];
+            passwordJoin.push(newNumber);
+            // console.log(newNumber);
         } else {
             return;
         }
     }
 
-    var underCaseConfirm = alert("Do you want undercase letters in your password?");
+console.log(finalPassword);
 
-    var upperCaseConfirm = alert("Do you want capitalized letters in your password?");
-
-    var specCharConfirm = alert("Do you want special characters in your password?");
-
-    var numberConfirm = alert("Do you want numbers in your password?");
-
-
+  for (var i = 0; i < confirmOpen; i++) {
+    var finalPassword = passwordJoin[Math.floor(Math.random() * passwordJoin.length)];
+    passwordJoin.push(finalPassword)
+    console.log (passwordJoin);
+    document.getElementById("final-password").innerHTML = passwordJoin.join(""); 
+  }
 }
+
     
 generateNumber();
